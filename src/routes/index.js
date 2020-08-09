93bd463fa7dc264daa212cb29fcb47c846f76486
Camilo Isaza <55111141+Andresmelek@users.noreply.github.com>
2020-08-09 17:22:06 -0500

    Commenting routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getToken, login, specialSearch, artistInfo } = require('./get')
 
+//Routes
 
 router.get('/token', getToken);
 router.get('/', login);
+//Endpoints
 router.get('/v1/special-search', specialSearch);
 router.post('/v1/artistsInfo', artistInfo)
 
