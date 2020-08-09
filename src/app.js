c8f5ff8327bb9900934efa03a44d6e64596f6612
Camilo Isaza <55111141+Andresmelek@users.noreply.github.com>
2020-08-09 17:21:17 -0500

    Commenting functions

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const app = express();
 
 
 
-
+//creation of the Express server
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);
 
 
-
+//Connection to the server port 3000
 app.listen(3000,()=>{
         console.log('Conected ...');
 });
